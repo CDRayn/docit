c20f0287a359db9eeb1deab0eaf250bb3bba5354
Clay Raynor <cd.raynor@gmail.com>
2018-04-29 17:10:12 -0400

    Add Logic To Pass Present Unit Tests
    Logic has been added to the file_handler.py module so that all current
    unit tests are now passing.

--- src/file_handler.py
+++ src/file_handler.py
@@ -1,3 +1,24 @@
 """
 This module is used for the handling of opening, parsing, and closing the .bash_history text file.
 """
+from os import path
+
+
+def inspect_file_path(file_path):
+    """
+    This function performs inspections of a file path to validates that file path
+    :param file_path: full path and filename to inspect
+    :type file_path: str
+    :return: 0 for valid file paths or a string for invalid file paths
+    :rtype: int or str
+    """
+
+    if not path.exists(file_path):
+        return f"File not found at {file_path}"
+
+    # Make sure the file is not empty
+    elif path.getsize(file_path) == 0:
+        return f"{file_path} is empty"
+
+    else:
+        return 0
--- tests/test_file_handler.py
+++ tests/test_file_handler.py
@@ -2,6 +2,10 @@
 This module contains unit tests for validating the file_handlers.py module.
 """
 import unittest
+import os
+
+
+from src.file_handler import inspect_file_path
 
 
 class TestFilePathValidator(unittest.TestCase):
@@ -13,9 +17,28 @@ class TestFilePathValidator(unittest.TestCase):
         """
         This test case checks the logic used to determine if a file exists at a give file path.
         """
+        empty_file = os.path.dirname(__file__) + '/test_files/empty_bash_history'
+        valid_file_path = os.path.dirname(__file__) + '/test_files/valid_bash_history'
+        invalid_file_path = '/this/path/does/not/exit'
 
-        # Return true for a valid file
-        self.assertTrue(None)
+        # Check that 0 is returned for a valid file
+        self.assertEqual(
+            inspect_file_path(valid_file_path),
+            0
+        )
+
+        # Check that a message stating the file can not be found is returned for invalid file paths
+        self.assertEqual(
+            inspect_file_path(invalid_file_path),
+            f"File not found at {invalid_file_path}"
+        )
+
+        # Check that a message stating the file is empty is returned for empty file paths
+        self.assertEqual(
+            inspect_file_path(empty_file),
+            f"{empty_file} is empty"
+
+        )
 
 
 if __name__ == '__main__':
