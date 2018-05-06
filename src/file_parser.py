8ad9066e375798e817ad86d2d7c15c2299198eef
Clay Raynor <cd.raynor@gmail.com>
2018-05-06 14:33:02 -0400

    Add Python module file_parser.py to repository
    The module file_parser.py was added to the repository. This module contains the
    logic used for parsing a .bash_history file and returning an in memory
    representation of the file.

---
+++ src/file_parser.py
@@ -0,0 +1,30 @@
+"""
+This module provides functionality for parsing an ASCII encoded .history_file and generating an in memory object that
+can be searched and evaluated.
+"""
+import os
+
+
+class BashHistFile:
+    """
+    This class contains the methods and properties of an in memory object representing a .bash_history file
+    """
+    def __init__(self, file_path):
+        """
+        This is the constructor method of the class.
+        :param file_path: fully qualified or relative file path to the .bash_history file
+        :type file_path: str
+        :returns: none
+        """
+        self.file_path = file_path
+        self.lines = []
+
+    def read_bash_hist_file(self):
+        """
+        This method reads a .bash_history_file and stores each line in a list
+        """
+        with open(self.file_path, mode='r') as file:
+            data = file.read()
+
+        for line_of_data in data:
+            self.lines.append(line_of_data)
